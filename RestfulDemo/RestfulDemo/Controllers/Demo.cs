c25f136cd51cbb1d79b45d215dcbe4045e651949
Om619 <94357281+Om619@users.noreply.github.com>
2022-02-14 10:14:29 +0530

    Add files via upload

---
+++ RestfulDemo/RestfulDemo/Controllers/Demo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Specialized;
+
+namespace RestfulDemo.Controllers
+{
+    [Route("api/Demo")]
+    [ApiController]
+    public class Demo : ControllerBase
+    {
+        [HttpPost("GetName")]
+        public async Task TestMethod([FromBody]HttpRequestMessage request)
+        //public string Hell()
+        {
+            //return "Hello";
+            try
+            {
+                if (request != null)
+                {
+                    var test = request.Content.ReadAsStringAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
